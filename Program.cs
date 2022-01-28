f9f24af0418764fe261707d3887e6d82431537e2
pcc98 <pcc98@LAPTOP-J4SA4RP1>
2022-01-28 15:44:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Pierce Carnell
+// 1/27/2022
+// ISM 4300
+
+using System;
+
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If you drive by looking at your rear view mirror, you're going to crash. The same applies to living life. - Pierce Carnell");
+        }
+    }
+}
